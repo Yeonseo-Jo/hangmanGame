9864e3f1a86a6608160b369d805dae20c20f77a4
Yeonseo-Jo <jys073190@gmail.com>
2023-06-26 15:36:42 +0900

    feat : 단어 랜덤 선택, 단어 선택 후 밑줄로 보여주기 구현

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const WORD_LIST = ["gosopt", "javascript", "borimong", "yeonseo", "webpart"];
+const chooseWord = () => {
+  const WORD_LIST = ["gosopt", "javascript", "borimong", "yeonseo", "webpart"];
+  const TARGET_WORD = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+
+  return TARGET_WORD;
+};
+
+// 랜덤으로 골라진 단어
+const answerWords = chooseWord().split("");
+
+// 단어 수에 맞게 밑줄 나타내기
+const showBlindedAnswer = () => {
+  const underLineUl = document.querySelector(".answer__letter");
+  console.log(underLineUl);
+
+  answerWords.forEach(() => {
+    let underLineLi = document.createElement("li");
+    underLineLi.textContent = "_";
+    underLineUl.appendChild(underLineLi);
+  });
+};
+
+window.onload = () => {
+  console.log(answerWords);
+  showBlindedAnswer();
+};
